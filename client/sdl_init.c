da8539096dde7bc43a1c86d5e73a5ea8748ffb75
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-11-28 17:41:14 +0100

    Add SDL initialization and cleanup functions
    Implement SDL initialization and cleanup functions.

---
+++ client/sdl_init.c
@@ -0,0 +1,28 @@
+#include "client.h"
+#include "../common/utils.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+// Init SDL + Font system
+SDL_Window* init_sdl() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        die("SDL Init failed, check SDL install maybe?");
+
+    if (TTF_Init() != 0)
+        die("Font system didn't start, TTF_Init error");
+
+    SDL_Window *win = SDL_CreateWindow("Hangman Race", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    if (!win)
+        die("Window didn't spawn, SDL_CreateWindow error");
+
+    return win;
+}
+
+// Shut everything down cleanly
+void cleanup_sdl(SDL_Window* win, SDL_Renderer* ren) {
+    if (ren) SDL_DestroyRenderer(ren);
+    if (win) SDL_DestroyWindow(win);
+
+    TTF_Quit();
+    SDL_Quit();
+}
