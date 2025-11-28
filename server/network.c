7487e18f0d52d1e077bd1ff4c5e47ab696bc5f7c
SergeGod <serge1hagopian@gmail.com>
2025-11-28 19:15:34 +0100

    Update network.c

---
+++ server/network.c
@@ -0,0 +1,26 @@
+#include "server.h"
+#include "../common/utils.h"
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int create_server_socket(int port) {
+    int server_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        die("setsockopt");
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+        die("setsockopt");
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+        die("listen");
+
+    return server_fd;
+}
