00670e60e97c21d0a0c8007af32f423cf668e8df
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-11-28 22:34:13 +0100

    Update GameState struct and add render_text function
    Added a new field 'word_len' to the GameState struct and declared a new function 'render_text'.

--- client/client.h
+++ client/client.h
@@ -18,11 +18,13 @@ typedef struct {
     char status_msg[128];
     int game_over;
     char guessed_letters[27];
+    int word_len;
 
     pthread_mutex_t state_mutex;
 } GameState;
 
 extern GameState state;
+void render_text(SDL_Renderer *ren, TTF_Font *font, const char *txt, int x, int y, SDL_Color color);
 
 // SDL setup / cleanup
 SDL_Window* init_sdl();
@@ -36,6 +38,7 @@ void *network_listen_thread(void *arg);
 void send_guess(char letter);
 
 #endif
+
 
 
 
