5ac1c96362a0a7ee4a32d044225b8725caf25d15
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-11-28 23:47:48 +0100

    Update server.h

--- server/server.h
+++ server/server.h
@@ -3,6 +3,7 @@
 
 #include <netinet/in.h>
 #include <pthread.h>
+#include <time.h>
 #include "../common/protocol.h"
 
 int create_server_socket(int port);
@@ -13,7 +14,6 @@ typedef struct {
     int id;
     int active;
     
-    // Game State
     int level;
     char current_word[64];
     char masked_word[64];
@@ -25,18 +25,13 @@ typedef struct {
     int finished;
 } Client;
 
-// Global State
 extern Client *clients[100];
 extern int client_count;
 extern pthread_mutex_t clients_mutex;
 
-// Functions
 void init_words();
 char* get_random_word(int length);
 void update_leaderboard(int client_id, const char* result);
 void broadcast(const char* message);
 
 #endif
-
-
-
