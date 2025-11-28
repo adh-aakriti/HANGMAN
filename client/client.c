c87ee947a4849bd498950effa62676e0228f1d83
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-11-28 19:40:10 +0100

    Refactor client connection and SDL initialization
    Refactored client code to improve structure and error handling. Updated connection logic and SDL setup for better performance.

--- client/client.c
+++ client/client.c
@@ -1,119 +1,101 @@
 #include "client.h"
 #include "../common/utils.h"
+
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 GameState state;
 
-void connect_to_server(char* ip, int port) 
-{
-    struct sockaddr_in serv_addr;
-
-    state.socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (state.socket_fd < 0)
-    {
-        printf("Error creating socket!\n");
-        exit(1);
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)
-    {
-        printf("Invalid IP address.\n");
-        exit(1)
-    }
-
-    if (connect(state.socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("Could not connect to server.\n");
-        exit(1)
-    }
-}
-
-int main(int argc, char *argv[]) 
-{
-    if (argc < 2) 
-    {
-        printf("Usage: %s <server_ip>\n", argv[0]);
+static int connect_to_server(void) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
         return -1;
     }
 
-    // Init State
-    state.running = 1;
-    state.level = 1;
-    state.mistakes = 0;
-    state.timer_val = 120;
-    strcpy(state.status_msg, "Connecting...");
-    pthread_mutex_init(&state.state_mutex, NULL);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);      
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); 
 
-    // SDL Setup
-    SDL_Window *win = init_sdl();
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-
-    // NOTE: Ensure a .ttf file exists in assets or root
-    TTF_Font *font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
-    if (!font) 
-    {
-        printf("Font not found, trying local file...\n");
-        font = TTF_OpenFont("assets/font.ttf", 24);
-        
-        if(!font)
-        {
-            printf("Failed to load any font!! Put a .ttf in assets/\n");
-            return 1;
-        }
+    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        close(fd);
+        return -1;
     }
 
-    // Network Setup
-    connect_to_server(argv[1], PORT);
+    return fd;
+}
+
+int main(void) {
+    memset(&state, 0, sizeof(state));
+    pthread_mutex_init(&state.state_mutex, NULL);
+
+    state.socket_fd = connect_to_server();
+    if (state.socket_fd < 0) {
+        fprintf(stderr, "could not connect to server\n");
+        return 1;
+    }
+
+    state.running = 1;
+
     pthread_t net_thread;
-    pthread_create(&net_thread, NULL, network_listen_thread, NULL);
+    if (pthread_create(&net_thread, NULL, network_listen_thread, NULL) != 0) {
+        perror("pthread_create");
+        close(state.socket_fd);
+        return 1;
+    }
 
-    //Main Loop
-    SDL_Event event;
-    Uint32 last_tick = SDL_GetTicks();
+    SDL_Window *win = init_sdl();
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!ren) die("renderer failed");
 
-    while (state.running) 
-    {
-        while (SDL_PollEvent(&event)) 
-        {
-            if (e.type == SDL_QUIT) 
-            {
+
+    TTF_Font *font = TTF_OpenFont("assets/FreeSans.ttf", 24);
+    if (!font) {
+
+        fprintf(stderr, "could not open font: %s\n", TTF_GetError());
+    }
+
+    SDL_Event ev;
+    Uint32 last_timer_tick = SDL_GetTicks();
+
+    while (state.running && !state.game_over) {
+
+        while (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_QUIT) {
                 state.running = 0;
-            }
-            else if (event.type == SDL_KEYDOWN) 
-            {
-                char key = event.key.keysym.sym;
-                if (k >= 'a' && k <= 'z') 
-                {
-                    send_guess(k);
+            } else if (ev.type == SDL_KEYDOWN) {
+                SDL_Keycode key = ev.key.keysym.sym;
+
+                if (key >= SDLK_a && key <= SDLK_z) {
+                    char letter = (char)('a' + (key - SDLK_a));
+                    send_guess(letter);
                 }
             }
         }
 
-        // Local Timer Countdown (Syncs with server roughly)
         Uint32 now = SDL_GetTicks();
-        if (now - last_timer_tick >= 1000) 
-        {
-            pthread_mutex_lock(&state.state_mutex);
-            
-            if (state.timer_val > 0 && state.game_over) 
-            {
-                state.timer_val--;
-            }
-            pthread_mutex_unlock(&state.state_mutex);
-            
-            last_tick = now;
+        if (now - last_timer_tick > 1000) {
+            last_timer_tick = now;
         }
 
         render_game(ren, font);
-        
-        SDL_Delay(16); // 60 FPS
+        SDL_Delay(16);
     }
+
+    state.running = 0;
+    pthread_join(net_thread, NULL);
+
     close(state.socket_fd);
+
+    if (font) TTF_CloseFont(font);
     cleanup_sdl(win, ren);
+    pthread_mutex_destroy(&state.state_mutex);
+
     return 0;
 }
-
