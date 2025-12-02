158b9946ce725eb1c9d3f9e6f7d5f3a3f57f16de
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 20:38:53 +0100

    Refactor utils.c with added comments
    Refactor die and trim_newline functions with comments.

--- common/utils.c
+++ common/utils.c
@@ -1,6 +1,6 @@
-#include "utils.h"
+#include "utils.h" //includes utils.c so it knows the functions
 
-void die(const char *msg) {
+void die(const char *msg) { //prints an the error if there is one
     perror(msg);
     exit(EXIT_FAILURE);
 }
@@ -8,12 +8,13 @@ void trim_newline(char *str) {
     
     if (!str) return;
 
-    for (char *p = str; *p; ++p) {
+    for (char *p = str; *p; ++p) { // declares pointer p starting at str and ending when *p = \0
         
-        if (*p == '\n' || *p == '\r') {
+        if (*p == '\n' || *p == '\r') { 
             *p = '\0';
             break;
         }
     }
 }
+
 
