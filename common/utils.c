3e792140c5dd22c5f265d5d27a9fed6ca585d7e8
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 17:10:27 +0100

    Update utils.c

--- common/utils.c
+++ common/utils.c
@@ -4,14 +4,16 @@ void die(const char *msg) {
     perror(msg);
     exit(EXIT_FAILURE);
 }
-
 void trim_newline(char *str) {
+    
     if (!str) return;
 
     for (char *p = str; *p; ++p) {
+        
         if (*p == '\n' || *p == '\r') {
             *p = '\0';
             break;
         }
     }
 }
+
