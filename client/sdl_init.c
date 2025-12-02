e082c0952dfc9dded6b4c829d33d074761e7e4e3
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 17:09:38 +0100

    Update sdl_init.c

--- client/sdl_init.c
+++ client/sdl_init.c
@@ -3,29 +3,32 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 
-// Init SDL + Font system
 SDL_Window* init_sdl() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
         die("SDL Init failed, check SDL install maybe?");
+    }
 
-    if (TTF_Init() != 0)
+    if (TTF_Init() != 0){
         die("Font system didn't start, TTF_Init error");
-
+    }
     SDL_Window *win = SDL_CreateWindow("Hangman Race", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-    if (!win)
+    
+    if (!win){
         die("Window didn't spawn, SDL_CreateWindow error");
-
+    }
     return win;
 }
-
-// Shut everything down cleanly
-void cleanup_sdl(SDL_Window* win, SDL_Renderer* ren) {
-    if (ren) SDL_DestroyRenderer(ren);
-    if (win) SDL_DestroyWindow(win);
-
+void cleanup_sdl(SDL_Window* win, SDL_Renderer* ren){
+    if (ren) {
+        SDL_DestroyRenderer(ren);
+    }
+    if (win){ 
+        SDL_DestroyWindow(win);
+    }
     TTF_Quit();
     SDL_Quit();
 }
+
 
 
 
