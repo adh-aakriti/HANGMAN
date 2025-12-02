362a48dcf1442c34ebb38687fec6275b4d150859
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 17:10:07 +0100

    Reformat protocol.h for consistency

--- common/protocol.h
+++ common/protocol.h
@@ -1,17 +1,11 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
-
 #define PORT 8080
 #define BUFFER_SIZE 1024
-
-// Game Constants
 #define MAX_LIVES 6
 #define NUM_LEVELS 3
-
-// Protocol Headers
 #define CMD_READY "READY"
 #define CMD_GUESS "GUESS"
-
 #define MSG_WELCOME "WELCOME" // CONNECTION ACK
 #define MSG_LEVEL "LEVEL"     // LEVEL <n>
 #define MSG_WORD "WORD"       // WORD <masked>
@@ -21,5 +15,5 @@
 #define MSG_NEWWORD "NEW_WORD" // NEW_WORD <masked>
 #define MSG_WIN "WINNER"      // WINNER <name>
 #define MSG_GAMEOVER "GAME_OVER"
-
 #endif
+
