50fd0d97cb9ad1bd023a1bbc40e6876d2d00fd40
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 20:29:11 +0100

    Update protocol.h

--- common/protocol.h
+++ common/protocol.h
@@ -1,19 +1,20 @@
-#ifndef PROTOCOL_H
-#define PROTOCOL_H
+#ifndef PROTOCOL_H //if protocol was already defined it will skip to below
+#define PROTOCOL_H 
 #define PORT 8080
 #define BUFFER_SIZE 1024
-#define MAX_LIVES 6
+#define MAX_LIVES 6  //we actually use 7 so not really enforced 
 #define NUM_LEVELS 3
 #define CMD_READY "READY"
 #define CMD_GUESS "GUESS"
-#define MSG_WELCOME "WELCOME" // CONNECTION ACK
-#define MSG_LEVEL "LEVEL"     // LEVEL <n>
-#define MSG_WORD "WORD"       // WORD <masked>
-#define MSG_TIMER "TIMER"     // TIMER <seconds>
-#define MSG_UPDATE "UPDATE"   // UPDATE <masked> <mistakes>
+#define MSG_WELCOME "WELCOME" 
+#define MSG_LEVEL "LEVEL"    
+#define MSG_WORD "WORD"    
+#define MSG_TIMER "TIMER"   
+#define MSG_UPDATE "UPDATE"   
 #define MSG_TIMEUP "TIME_UP"  
-#define MSG_NEWWORD "NEW_WORD" // NEW_WORD <masked>
-#define MSG_WIN "WINNER"      // WINNER <name>
+#define MSG_NEWWORD "NEW_WORD" 
+#define MSG_WIN "WINNER"    
 #define MSG_GAMEOVER "GAME_OVER"
-#endif
+#endif // Here <<<<<------
+
 
