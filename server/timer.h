42ca526dea5af28aa0e364d308c723f075cc6ec1
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 18:16:53 +0100

    Update timer.h

--- server/timer.h
+++ server/timer.h
@@ -1,13 +1,12 @@
 #ifndef TIMER_H
 #define TIMER_H
-
 #include <time.h>
 
 typedef struct {
     time_t start;
     int dur;
-} GameTimer;
-
+} 
+GameTimer;
 void timer_start(GameTimer *t, int dur);
 int timer_is_expired(GameTimer *t);
 int timer_get_remaining(GameTimer *t);
