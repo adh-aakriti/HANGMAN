a886e7679594758a35499262c46e8718c7e22d9a
Amit Mayer <amayer.ieu2024@student.ie.edu>
2025-12-02 20:34:44 +0100

    Fix header guard formatting in utils.h

--- common/utils.h
+++ common/utils.h
@@ -1,9 +1,10 @@
-#ifndef UTILS_H
+#ifndef UTILS_H // same check for if utils is defined
 #define UTILS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 void die(const char *msg);
 void trim_newline(char *str);
-#endif
+#endif // ends here <<<<----
+
 
